174968ef93fbf0d8c6832a6ef3398c546cdc3831
NandaSasidharan <sr.nanda.k@gmail.com>
2024-04-07 13:14:01 +0200

    set up for a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='aiimc',
+    packages=find_packages(include=['aiimc']),
+    version='0.1.0',
+    description='package to simulate AI in memory computing',
+    author='Nanda',
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[],
+    test_suite='',
+)
